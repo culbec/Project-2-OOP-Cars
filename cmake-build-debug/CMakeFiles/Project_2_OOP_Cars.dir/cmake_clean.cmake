d96556c6a77d63df4d050bbb655d1c8b11b20573
culbec <profir.alexx@gmail.com>
2023-04-28 14:47:06 +0300

    Updated Lab7 branch.

---
+++ cmake-build-debug/CMakeFiles/Project_2_OOP_Cars.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Car.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Car.cpp.o.d"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Repository.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Repository.cpp.o.d"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Service.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Service.cpp.o.d"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/UI.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/UI.cpp.o.d"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Validator.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Validator.cpp.o.d"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/tests.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/Sources/tests.cpp.o.d"
+  "CMakeFiles/Project_2_OOP_Cars.dir/main.cpp.o"
+  "CMakeFiles/Project_2_OOP_Cars.dir/main.cpp.o.d"
+  "Project_2_OOP_Cars"
+  "Project_2_OOP_Cars.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_2_OOP_Cars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Project_2_OOP_Cars.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Project_2_OOP_Cars.
+# This may be replaced when dependencies are built.
