2012b5b5bc378e38f14093a7bd0a4c9dd3a05749
culbec <profir.alexx@gmail.com>
2023-04-28 10:09:26 +0300

    Fixing

---
+++ cmake-build-debug/CMakeFiles/Project_2_OOP_Cars.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/culbec/OOP/Project-2-OOP-Cars/Sources/Car.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Car.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Car.cpp.o.d"
+  "/home/culbec/OOP/Project-2-OOP-Cars/Sources/Repository.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Repository.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Repository.cpp.o.d"
+  "/home/culbec/OOP/Project-2-OOP-Cars/Sources/Service.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Service.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Service.cpp.o.d"
+  "/home/culbec/OOP/Project-2-OOP-Cars/Sources/UI.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/UI.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/UI.cpp.o.d"
+  "/home/culbec/OOP/Project-2-OOP-Cars/Sources/Validator.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Validator.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/Validator.cpp.o.d"
+  "/home/culbec/OOP/Project-2-OOP-Cars/Sources/tests.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/tests.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/Sources/tests.cpp.o.d"
+  "/home/culbec/OOP/Project-2-OOP-Cars/main.cpp" "CMakeFiles/Project_2_OOP_Cars.dir/main.cpp.o" "gcc" "CMakeFiles/Project_2_OOP_Cars.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
